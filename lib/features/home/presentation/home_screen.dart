cb841fc0ac14af76a2839cddfac1048a122d9095
Majid Bhuiyan <majid.bhuiyan2011@gmail.com>
2025-11-26 15:08:28 +0600

    init smart edu

---
+++ lib/features/home/presentation/home_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:  Text('SmartEdu'),
+        centerTitle: false,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.settings),
+          ),
+          IconButton(onPressed: () {},
+          icon: const Icon(Icons.menu),)
+        ],
+      ),
+    );
+  }
+}
